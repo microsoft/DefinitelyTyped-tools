11b9cb30a9a62cec5ab1c5cf47d2809b3b66e172
Andy <anhans@microsoft.com>
2018-07-09 10:07:23 -0700

    Merge pull request #14 from Ailrun/add-2.9
    Add 2.9

--- packages/definitelytyped-header-parser/index.ts
+++ packages/definitelytyped-header-parser/index.ts
@@ -10,9 +10,9 @@ Example:
 */
 
 /** Parse-able TypeScript versions. Only add to this list if we will support this version on DefinitelyTyped. */
-export type TypeScriptVersion = "2.0" | "2.1" | "2.2" | "2.3" | "2.4" | "2.5" | "2.6" | "2.7" | "2.8";
+export type TypeScriptVersion = "2.0" | "2.1" | "2.2" | "2.3" | "2.4" | "2.5" | "2.6" | "2.7" | "2.8" | "2.9";
 export namespace TypeScriptVersion {
-	export const all: ReadonlyArray<TypeScriptVersion> = ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8"];
+	export const all: ReadonlyArray<TypeScriptVersion> = ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9"];
 	export const lowest = all[0];
 	/** Latest version that may be specified in a `// TypeScript Version:` header. */
 	export const latest = all[all.length - 1];
