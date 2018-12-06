87c64edb76cd6f0ecf8849fb98d5b9dd53548864
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-12-06 14:38:46 -0800

    Merge pull request #19 from Microsoft/add-3.3
    Add Typescript 3.3

--- packages/definitelytyped-header-parser/index.ts
+++ packages/definitelytyped-header-parser/index.ts
@@ -12,10 +12,10 @@ Example:
 
 /** Parse-able TypeScript versions. Only add to this list if we will support this version on DefinitelyTyped. */
 export type TypeScriptVersion =
-	"2.0" | "2.1" | "2.2" | "2.3" | "2.4" | "2.5" | "2.6" | "2.7" | "2.8" | "2.9" | "3.0" | "3.1" | "3.2";
+	"2.0" | "2.1" | "2.2" | "2.3" | "2.4" | "2.5" | "2.6" | "2.7" | "2.8" | "2.9" | "3.0" | "3.1" | "3.2" | "3.3";
 export namespace TypeScriptVersion {
 	export const all: ReadonlyArray<TypeScriptVersion> =
-		["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2"];
+		["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3"];
 	export const lowest = all[0];
 	/** Latest version that may be specified in a `// TypeScript Version:` header. */
 	export const latest = all[all.length - 1];
@@ -43,6 +43,7 @@ export namespace TypeScriptVersion {
 		"ts3.0",
 		"ts3.1",
 		"ts3.2",
+		"ts3.3",
 		"latest",
 	];
 
