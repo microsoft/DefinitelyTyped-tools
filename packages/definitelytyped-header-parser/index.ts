607eea764cf11e7221b4184166018cd4818fe897
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-03-29 10:30:03 -0700

    Merge pull request #22 from Microsoft/add-3.5
    Add 3.5

--- packages/definitelytyped-header-parser/index.ts
+++ packages/definitelytyped-header-parser/index.ts
@@ -12,158 +12,159 @@ Example:
 
 /** Parse-able TypeScript versions. Only add to this list if we will support this version on DefinitelyTyped. */
 export type TypeScriptVersion =
-	"2.0" | "2.1" | "2.2" | "2.3" | "2.4" | "2.5" | "2.6" | "2.7" | "2.8" | "2.9" | "3.0" | "3.1" | "3.2" | "3.3" | "3.4";
+    "2.0" | "2.1" | "2.2" | "2.3" | "2.4" | "2.5" | "2.6" | "2.7" | "2.8" | "2.9" | "3.0" | "3.1" | "3.2" | "3.3" | "3.4" | "3.5";
 export namespace TypeScriptVersion {
-	export const all: ReadonlyArray<TypeScriptVersion> =
-		["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3", "3.4"];
-	export const lowest = all[0];
-	/** Latest version that may be specified in a `// TypeScript Version:` header. */
-	export const latest = all[all.length - 1];
+    export const all: ReadonlyArray<TypeScriptVersion> =
+        ["2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7", "2.8", "2.9", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5"];
+    export const lowest = all[0];
+    /** Latest version that may be specified in a `// TypeScript Version:` header. */
+    export const latest = all[all.length - 1];
 
-	/** @deprecated */
-	export function isPrerelease(_version: TypeScriptVersion): boolean {
-		return false;
-	}
+    /** @deprecated */
+    export function isPrerelease(_version: TypeScriptVersion): boolean {
+        return false;
+    }
 
-	export function range(min: TypeScriptVersion): ReadonlyArray<TypeScriptVersion> {
-		return all.filter(v => v >= min);
-	}
+    export function range(min: TypeScriptVersion): ReadonlyArray<TypeScriptVersion> {
+        return all.filter(v => v >= min);
+    }
 
-	const allTags: ReadonlyArray<string> = [
-		"ts2.0",
-		"ts2.1",
-		"ts2.2",
-		"ts2.3",
-		"ts2.4",
-		"ts2.5",
-		"ts2.6",
-		"ts2.7",
-		"ts2.8",
-		"ts2.9",
-		"ts3.0",
-		"ts3.1",
-		"ts3.2",
-		"ts3.3",
-		"ts3.4",
-		"latest",
-	];
+    const allTags: ReadonlyArray<string> = [
+        "ts2.0",
+        "ts2.1",
+        "ts2.2",
+        "ts2.3",
+        "ts2.4",
+        "ts2.5",
+        "ts2.6",
+        "ts2.7",
+        "ts2.8",
+        "ts2.9",
+        "ts3.0",
+        "ts3.1",
+        "ts3.2",
+        "ts3.3",
+        "ts3.4",
+        "ts3.5",
+        "latest",
+    ];
 
-	/** List of NPM tags that should be changed to point to the latest version. */
-	export function tagsToUpdate(typeScriptVersion: TypeScriptVersion): ReadonlyArray<string>  {
-		// A 2.0-compatible package is assumed compatible with TypeScript 2.1
-		// We want the "2.1" tag to always exist.
-		const idx = allTags.indexOf(`ts${typeScriptVersion}`);
-		if (idx === -1) { throw new Error(); }
-		return allTags.slice(idx);
-	}
+    /** List of NPM tags that should be changed to point to the latest version. */
+    export function tagsToUpdate(typeScriptVersion: TypeScriptVersion): ReadonlyArray<string>  {
+        // A 2.0-compatible package is assumed compatible with TypeScript 2.1
+        // We want the "2.1" tag to always exist.
+        const idx = allTags.indexOf(`ts${typeScriptVersion}`);
+        if (idx === -1) { throw new Error(); }
+        return allTags.slice(idx);
+    }
 
-	export function previous(v: TypeScriptVersion): TypeScriptVersion | undefined {
-		const index = all.indexOf(v);
-		assert(index !== -1);
-		return index === 0 ? undefined : all[index - 1];
-	}
+    export function previous(v: TypeScriptVersion): TypeScriptVersion | undefined {
+        const index = all.indexOf(v);
+        assert(index !== -1);
+        return index === 0 ? undefined : all[index - 1];
+    }
 
-	export function isRedirectable(v: TypeScriptVersion): boolean {
-		return all.indexOf(v) >= all.indexOf("3.1");
-	}
+    export function isRedirectable(v: TypeScriptVersion): boolean {
+        return all.indexOf(v) >= all.indexOf("3.1");
+    }
 }
 
 export interface Header {
-	readonly nonNpm: boolean;
-	readonly libraryName: string;
-	readonly libraryMajorVersion: number;
-	readonly libraryMinorVersion: number;
-	readonly typeScriptVersion: TypeScriptVersion;
-	readonly projects: ReadonlyArray<string>;
-	readonly contributors: ReadonlyArray<Author>;
+    readonly nonNpm: boolean;
+    readonly libraryName: string;
+    readonly libraryMajorVersion: number;
+    readonly libraryMinorVersion: number;
+    readonly typeScriptVersion: TypeScriptVersion;
+    readonly projects: ReadonlyArray<string>;
+    readonly contributors: ReadonlyArray<Author>;
 }
 
 export interface Author {
-	readonly name: string;
-	readonly url: string;
-	readonly githubUsername: string | undefined;
+    readonly name: string;
+    readonly url: string;
+    readonly githubUsername: string | undefined;
 }
 
 export interface ParseError {
-	readonly index: number;
-	readonly line: number;
-	readonly column: number;
-	readonly expected: ReadonlyArray<string>;
+    readonly index: number;
+    readonly line: number;
+    readonly column: number;
+    readonly expected: ReadonlyArray<string>;
 }
 
 export function isTypeScriptVersion(str: string): str is TypeScriptVersion {
-	return TypeScriptVersion.all.includes(str as TypeScriptVersion);
+    return TypeScriptVersion.all.includes(str as TypeScriptVersion);
 }
 
 export function makeTypesVersionsForPackageJson(typesVersions: ReadonlyArray<TypeScriptVersion>): unknown {
-	if (typesVersions.length === 0) { return undefined; }
+    if (typesVersions.length === 0) { return undefined; }
 
-	const out: { [key: string]: { readonly "*": ReadonlyArray<string> } } = {};
-	for (const version of typesVersions) {
-		out[`>=${version}.0-0`] = { "*": [`ts${version}/*`] };
-	}
-	return out;
+    const out: { [key: string]: { readonly "*": ReadonlyArray<string> } } = {};
+    for (const version of typesVersions) {
+        out[`>=${version}.0-0`] = { "*": [`ts${version}/*`] };
+    }
+    return out;
 }
 
 export function parseHeaderOrFail(mainFileContent: string): Header {
-	const header = parseHeader(mainFileContent, /*strict*/false);
-	if (isParseError(header)) {
-		throw new Error(renderParseError(header));
-	}
-	return header;
+    const header = parseHeader(mainFileContent, /*strict*/false);
+    if (isParseError(header)) {
+        throw new Error(renderParseError(header));
+    }
+    return header;
 }
 
 export function validate(mainFileContent: string): ParseError | undefined {
-	const h = parseHeader(mainFileContent, /*strict*/true);
-	return isParseError(h) ? h : undefined;
+    const h = parseHeader(mainFileContent, /*strict*/true);
+    return isParseError(h) ? h : undefined;
 }
 
 export function renderExpected(expected: ReadonlyArray<string>): string {
-	return expected.length === 1 ? expected[0] : `one of\n\t${expected.join("\n\t")}`;
+    return expected.length === 1 ? expected[0] : `one of\n\t${expected.join("\n\t")}`;
 }
 
 function renderParseError({ line, column, expected }: ParseError): string {
-	return `At ${line}:${column} : Expected ${renderExpected(expected)}`;
+    return `At ${line}:${column} : Expected ${renderExpected(expected)}`;
 }
 
 function isParseError(x: {}): x is ParseError {
-	// tslint:disable-next-line strict-type-predicates
-	return (x as ParseError).expected !== undefined;
+    // tslint:disable-next-line strict-type-predicates
+    return (x as ParseError).expected !== undefined;
 }
 
 /** @param strict If true, we allow fewer things to be parsed. Turned on by linting. */
 function parseHeader(text: string, strict: boolean): Header | ParseError {
-	const res = headerParser(strict).parse(text);
-	return res.status
-		? res.value
-		: { index: res.index.offset, line: res.index.line, column: res.index.column, expected: res.expected };
+    const res = headerParser(strict).parse(text);
+    return res.status
+        ? res.value
+        : { index: res.index.offset, line: res.index.line, column: res.index.column, expected: res.expected };
 }
 
 function headerParser(strict: boolean): pm.Parser<Header> {
-	return pm.seqMap(
-		pm.regex(/\/\/ Type definitions for (non-npm package )?/),
-		parseLabel(strict),
-		pm.string("// Project: "),
-		projectParser,
-		pm.regexp(/\r?\n\/\/ Definitions by: /),
-		contributorsParser(strict),
-		definitionsParser,
-		typeScriptVersionParser,
-		pm.all, // Don't care about the rest of the file
-		// tslint:disable-next-line:variable-name
-		(str, label, _project, projects, _defsBy, contributors, _definitions, typeScriptVersion) => ({
-			libraryName: label.name,
-			libraryMajorVersion: label.major,
-			libraryMinorVersion: label.minor,
-			nonNpm: str.endsWith("non-npm package "),
-			projects, contributors, typeScriptVersion,
-		}));
+    return pm.seqMap(
+        pm.regex(/\/\/ Type definitions for (non-npm package )?/),
+        parseLabel(strict),
+        pm.string("// Project: "),
+        projectParser,
+        pm.regexp(/\r?\n\/\/ Definitions by: /),
+        contributorsParser(strict),
+        definitionsParser,
+        typeScriptVersionParser,
+        pm.all, // Don't care about the rest of the file
+        // tslint:disable-next-line:variable-name
+        (str, label, _project, projects, _defsBy, contributors, _definitions, typeScriptVersion) => ({
+            libraryName: label.name,
+            libraryMajorVersion: label.major,
+            libraryMinorVersion: label.minor,
+            nonNpm: str.endsWith("non-npm package "),
+            projects, contributors, typeScriptVersion,
+        }));
 }
 
 interface Label {
-	readonly name: string;
-	readonly major: number;
-	readonly minor: number;
+    readonly name: string;
+    readonly major: number;
+    readonly minor: number;
 }
 
 /*
@@ -184,127 +185,127 @@ const separator: pm.Parser<string> = pm.regexp(/(, )|(,?\r?\n\/\/\s\s+)/);
 const projectParser: pm.Parser<ReadonlyArray<string>> = pm.sepBy1(pm.regexp(/[^,\r\n]+/), separator);
 
 function contributorsParser(strict: boolean): pm.Parser<ReadonlyArray<Author>> {
-	const contributor: pm.Parser<Author> = strict
-		? pm.seqMap(
-			pm.regexp(/([^<]+) /, 1),
-			pm.regexp(/\<https\:\/\/github\.com\/([a-zA-Z\d\-]+)\>/, 1),
-			(name, githubUsername) => ({ name, url: `https://github.com/${githubUsername}`, githubUsername }))
-		// In non-strict mode, allows arbitrary URL, and trailing whitespace.
-		: pm.seqMap(pm.regexp(/([^<]+) /, 1), pm.regexp(/<([^>]+)> */, 1), (name, url) => {
-			const rgx = /^https\:\/\/github.com\/([a-zA-Z\d\-]+)$/;
-			const match = rgx.exec(url);
-			// tslint:disable-next-line no-null-keyword
-			return ({ name, url, githubUsername: match === null ? undefined : match[1] });
-		});
-	return pm.sepBy1(contributor, separator);
+    const contributor: pm.Parser<Author> = strict
+        ? pm.seqMap(
+            pm.regexp(/([^<]+) /, 1),
+            pm.regexp(/\<https\:\/\/github\.com\/([a-zA-Z\d\-]+)\>/, 1),
+            (name, githubUsername) => ({ name, url: `https://github.com/${githubUsername}`, githubUsername }))
+        // In non-strict mode, allows arbitrary URL, and trailing whitespace.
+        : pm.seqMap(pm.regexp(/([^<]+) /, 1), pm.regexp(/<([^>]+)> */, 1), (name, url) => {
+            const rgx = /^https\:\/\/github.com\/([a-zA-Z\d\-]+)$/;
+            const match = rgx.exec(url);
+            // tslint:disable-next-line no-null-keyword
+            return ({ name, url, githubUsername: match === null ? undefined : match[1] });
+        });
+    return pm.sepBy1(contributor, separator);
 }
 
 // TODO: Should we do something with the URL?
 const definitionsParser = pm.regexp(/\r?\n\/\/ Definitions: [^\r\n]+/);
 
 function parseLabel(strict: boolean): pm.Parser<Label> {
-	return pm.Parser((input, index) => {
-		// Take all until the first newline.
-		const endIndex = regexpIndexOf(input, /\r|\n/, index);
-		if (endIndex === -1) {
-			return fail("EOF");
-		}
-		// Index past the end of the newline.
-		const end = input[endIndex] === "\r" ? endIndex + 2 : endIndex + 1;
-		const tilNewline = input.slice(index, endIndex);
+    return pm.Parser((input, index) => {
+        // Take all until the first newline.
+        const endIndex = regexpIndexOf(input, /\r|\n/, index);
+        if (endIndex === -1) {
+            return fail("EOF");
+        }
+        // Index past the end of the newline.
+        const end = input[endIndex] === "\r" ? endIndex + 2 : endIndex + 1;
+        const tilNewline = input.slice(index, endIndex);
 
-		// Parse in reverse. Once we've stripped off the version, the rest is the libary name.
-		const reversed = reverse(tilNewline);
+        // Parse in reverse. Once we've stripped off the version, the rest is the libary name.
+        const reversed = reverse(tilNewline);
 
-		// Last digit is allowed to be "x", which acts like "0"
-		const rgx = /((\d+|x)\.(\d+)(\.\d+)?(v)? )?(.+)/;
-		const match = rgx.exec(reversed);
-		if (match === null) { // tslint:disable-line no-null-keyword
-			return fail();
-		}
-		const [, version, a, b, c, v, nameReverse] = match;
+        // Last digit is allowed to be "x", which acts like "0"
+        const rgx = /((\d+|x)\.(\d+)(\.\d+)?(v)? )?(.+)/;
+        const match = rgx.exec(reversed);
+        if (match === null) { // tslint:disable-line no-null-keyword
+            return fail();
+        }
+        const [, version, a, b, c, v, nameReverse] = match;
 
-		let majorReverse: string;
-		let minorReverse: string;
-		if (version !== undefined) { // tslint:disable-line strict-type-predicates
-			if (c !== undefined) { // tslint:disable-line strict-type-predicates
-				// There is a patch version
-				majorReverse = c;
-				minorReverse = b;
-				if (strict) {
-					return fail("patch version not allowed");
-				}
-			} else {
-				majorReverse = b;
-				minorReverse = a;
-			}
-			if (v !== undefined && strict) { // tslint:disable-line strict-type-predicates
-				return fail("'v' not allowed");
-			}
-		} else {
-			if (strict) {
-				return fail("Needs MAJOR.MINOR");
-			}
-			majorReverse = "0"; minorReverse = "0";
-		}
+        let majorReverse: string;
+        let minorReverse: string;
+        if (version !== undefined) { // tslint:disable-line strict-type-predicates
+            if (c !== undefined) { // tslint:disable-line strict-type-predicates
+                // There is a patch version
+                majorReverse = c;
+                minorReverse = b;
+                if (strict) {
+                    return fail("patch version not allowed");
+                }
+            } else {
+                majorReverse = b;
+                minorReverse = a;
+            }
+            if (v !== undefined && strict) { // tslint:disable-line strict-type-predicates
+                return fail("'v' not allowed");
+            }
+        } else {
+            if (strict) {
+                return fail("Needs MAJOR.MINOR");
+            }
+            majorReverse = "0"; minorReverse = "0";
+        }
 
-		const [name, major, minor] = [reverse(nameReverse), reverse(majorReverse), reverse(minorReverse)];
-		return pm.makeSuccess<Label>(end, { name, major: intOfString(major), minor: minor === "x" ? 0 : intOfString(minor) });
+        const [name, major, minor] = [reverse(nameReverse), reverse(majorReverse), reverse(minorReverse)];
+        return pm.makeSuccess<Label>(end, { name, major: intOfString(major), minor: minor === "x" ? 0 : intOfString(minor) });
 
-		function fail(msg?: string): pm.Reply<Label> {
-			let expected = "foo MAJOR.MINOR";
-			if (msg !== undefined) {
-				expected += ` (${msg})`;
-			}
-			return pm.makeFailure(index, expected);
-		}
-	});
+        function fail(msg?: string): pm.Reply<Label> {
+            let expected = "foo MAJOR.MINOR";
+            if (msg !== undefined) {
+                expected += ` (${msg})`;
+            }
+            return pm.makeFailure(index, expected);
+        }
+    });
 }
 
 const typeScriptVersionLineParser: pm.Parser<TypeScriptVersion> =
-	pm.regexp(/\/\/ TypeScript Version: (\d.(\d))/, 1).chain<TypeScriptVersion>(v =>
-		TypeScriptVersion.all.includes(v as TypeScriptVersion)
-			? pm.succeed(v as TypeScriptVersion)
-			: pm.fail(`TypeScript ${v} is not yet supported.`));
+    pm.regexp(/\/\/ TypeScript Version: (\d.(\d))/, 1).chain<TypeScriptVersion>(v =>
+        TypeScriptVersion.all.includes(v as TypeScriptVersion)
+            ? pm.succeed(v as TypeScriptVersion)
+            : pm.fail(`TypeScript ${v} is not yet supported.`));
 
 const typeScriptVersionParser: pm.Parser<TypeScriptVersion> =
-	pm.regexp(/\r?\n/)
-		.then(typeScriptVersionLineParser)
-		.fallback<TypeScriptVersion>("2.0");
+    pm.regexp(/\r?\n/)
+        .then(typeScriptVersionLineParser)
+        .fallback<TypeScriptVersion>("2.0");
 
 export function parseTypeScriptVersionLine(line: string): TypeScriptVersion {
-	const result = typeScriptVersionLineParser.parse(line);
-	if (!result.status) {
-		throw new Error(`Could not parse version: line is '${line}'`);
-	}
-	return result.value;
+    const result = typeScriptVersionLineParser.parse(line);
+    if (!result.status) {
+        throw new Error(`Could not parse version: line is '${line}'`);
+    }
+    return result.value;
 }
 
 function reverse(s: string): string {
-	let out = "";
-	for (let i = s.length - 1; i >= 0; i--) {
-		out += s[i];
-	}
-	return out;
+    let out = "";
+    for (let i = s.length - 1; i >= 0; i--) {
+        out += s[i];
+    }
+    return out;
 }
 
 function regexpIndexOf(s: string, rgx: RegExp, start: number): number {
-	const index = s.slice(start).search(rgx);
-	return index === -1 ? index : index + start;
+    const index = s.slice(start).search(rgx);
+    return index === -1 ? index : index + start;
 }
 
 declare module "parsimmon" {
-	// tslint:disable-next-line no-unnecessary-qualifier
-	type Pr<T> = pm.Parser<T>; // https://github.com/Microsoft/TypeScript/issues/14121
-	export function seqMap<T, U, V, W, X, Y, Z, A, B, C>(
-		p1: Pr<T>, p2: Pr<U>, p3: Pr<V>, p4: Pr<W>, p5: Pr<X>, p6: Pr<Y>, p7: Pr<Z>, p8: Pr<A>, p9: Pr<B>,
-		cb: (a1: T, a2: U, a3: V, a4: W, a5: X, a6: Y, a7: Z, a8: A, a9: B) => C): Pr<C>;
+    // tslint:disable-next-line no-unnecessary-qualifier
+    type Pr<T> = pm.Parser<T>; // https://github.com/Microsoft/TypeScript/issues/14121
+    export function seqMap<T, U, V, W, X, Y, Z, A, B, C>(
+        p1: Pr<T>, p2: Pr<U>, p3: Pr<V>, p4: Pr<W>, p5: Pr<X>, p6: Pr<Y>, p7: Pr<Z>, p8: Pr<A>, p9: Pr<B>,
+        cb: (a1: T, a2: U, a3: V, a4: W, a5: X, a6: Y, a7: Z, a8: A, a9: B) => C): Pr<C>;
 }
 
 function intOfString(str: string): number {
-	const n = Number.parseInt(str, 10);
-	if (Number.isNaN(n)) {
-		throw new Error(`Error in parseInt(${JSON.stringify(str)})`);
-	}
-	return n;
+    const n = Number.parseInt(str, 10);
+    if (Number.isNaN(n)) {
+        throw new Error(`Error in parseInt(${JSON.stringify(str)})`);
+    }
+    return n;
 }
--- packages/definitelytyped-header-parser/test/test.ts
+++ packages/definitelytyped-header-parser/test/test.ts
@@ -2,114 +2,114 @@ import assert = require("assert");
 import { parseHeaderOrFail, parseTypeScriptVersionLine, TypeScriptVersion } from "..";
 
 describe("parse", () => {
-	it("works", () => {
-		const src = dedent`
-			// Type definitions for foo 1.2
-			// Project: https://github.com/foo/foo, https://foo.com
-			// Definitions by: My Self <https://github.com/me>, Some Other Guy <https://github.com/otherguy>
-			// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-			// TypeScript Version: 2.2
+    it("works", () => {
+        const src = dedent`
+            // Type definitions for foo 1.2
+            // Project: https://github.com/foo/foo, https://foo.com
+            // Definitions by: My Self <https://github.com/me>, Some Other Guy <https://github.com/otherguy>
+            // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+            // TypeScript Version: 2.2
 
-			...file content...`;
-		assert.deepStrictEqual(parseHeaderOrFail(src), {
-			libraryName: "foo",
-			libraryMajorVersion: 1,
-			libraryMinorVersion: 2,
-			typeScriptVersion: "2.2",
-			nonNpm: false,
-			projects: ["https://github.com/foo/foo", "https://foo.com"],
-			contributors: [
-				{ name: "My Self", url: "https://github.com/me", githubUsername: "me" },
-				{ name: "Some Other Guy", url: "https://github.com/otherguy", githubUsername: "otherguy" },
-			],
-		});
-	});
+            ...file content...`;
+        assert.deepStrictEqual(parseHeaderOrFail(src), {
+            libraryName: "foo",
+            libraryMajorVersion: 1,
+            libraryMinorVersion: 2,
+            typeScriptVersion: "2.2",
+            nonNpm: false,
+            projects: ["https://github.com/foo/foo", "https://foo.com"],
+            contributors: [
+                { name: "My Self", url: "https://github.com/me", githubUsername: "me" },
+                { name: "Some Other Guy", url: "https://github.com/otherguy", githubUsername: "otherguy" },
+            ],
+        });
+    });
 
-	it("works with spacing", () => {
-		const src = dedent`
-			// Type definitions for foo 1.2
-			// Project: https://github.com/foo/foo,
-			//          https://foo.com
-			// Definitions by: My Self <https://github.com/me>,
-			//                 Some Other Guy <https://github.com/otherguy>
-			// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+    it("works with spacing", () => {
+        const src = dedent`
+            // Type definitions for foo 1.2
+            // Project: https://github.com/foo/foo,
+            //          https://foo.com
+            // Definitions by: My Self <https://github.com/me>,
+            //                 Some Other Guy <https://github.com/otherguy>
+            // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-			...file content...`;
+            ...file content...`;
 
-		assert.deepStrictEqual(parseHeaderOrFail(src), {
-			libraryName: "foo",
-			libraryMajorVersion: 1,
-			libraryMinorVersion: 2,
-			typeScriptVersion: "2.0",
-			nonNpm: false,
-			projects: ["https://github.com/foo/foo", "https://foo.com"],
-			contributors: [
-				{ name: "My Self", url: "https://github.com/me", githubUsername: "me" },
-				{ name: "Some Other Guy", url: "https://github.com/otherguy", githubUsername: "otherguy" },
-			],
-		});
-	});
+        assert.deepStrictEqual(parseHeaderOrFail(src), {
+            libraryName: "foo",
+            libraryMajorVersion: 1,
+            libraryMinorVersion: 2,
+            typeScriptVersion: "2.0",
+            nonNpm: false,
+            projects: ["https://github.com/foo/foo", "https://foo.com"],
+            contributors: [
+                { name: "My Self", url: "https://github.com/me", githubUsername: "me" },
+                { name: "Some Other Guy", url: "https://github.com/otherguy", githubUsername: "otherguy" },
+            ],
+        });
+    });
 
-	it("works with bad url", () => {
-		const src = dedent`
-			// Type definitions for foo 1.2
-			// Project: https://github.com/foo/foo
-			// Definitions by: Bad Url <sptth://hubgit.moc/em>
-			// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped`;
-		assert.deepStrictEqual(parseHeaderOrFail(src).contributors, [
-			{ name: "Bad Url", url: "sptth://hubgit.moc/em", githubUsername: undefined },
-		]);
-	});
+    it("works with bad url", () => {
+        const src = dedent`
+            // Type definitions for foo 1.2
+            // Project: https://github.com/foo/foo
+            // Definitions by: Bad Url <sptth://hubgit.moc/em>
+            // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped`;
+        assert.deepStrictEqual(parseHeaderOrFail(src).contributors, [
+            { name: "Bad Url", url: "sptth://hubgit.moc/em", githubUsername: undefined },
+        ]);
+    });
 
-	it("allows 'non-npm' on Type definitions line", () => {
-		const src = dedent`
-			// Type definitions for non-npm package foo 1.2
-			// Project: https://github.com/foo/foo, https://foo.com
-			// Definitions by: My Self <https://github.com/me>, Some Other Guy <https://github.com/otherguy>
-			// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-			// TypeScript Version: 2.2
+    it("allows 'non-npm' on Type definitions line", () => {
+        const src = dedent`
+            // Type definitions for non-npm package foo 1.2
+            // Project: https://github.com/foo/foo, https://foo.com
+            // Definitions by: My Self <https://github.com/me>, Some Other Guy <https://github.com/otherguy>
+            // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+            // TypeScript Version: 2.2
 
-			...file content...`;
-		console.log(parseHeaderOrFail(src))
-			assert.equal(parseHeaderOrFail(src).nonNpm, true);
-	});
+            ...file content...`;
+        console.log(parseHeaderOrFail(src))
+            assert.equal(parseHeaderOrFail(src).nonNpm, true);
+    });
 });
 
 describe("parseTypeScriptVersionLine", () => {
-	it("works", () => {
-		const src = "// TypeScript Version: 2.1";
-		assert.equal(parseTypeScriptVersionLine(src), "2.1");
+    it("works", () => {
+        const src = "// TypeScript Version: 2.1";
+        assert.equal(parseTypeScriptVersionLine(src), "2.1");
 
-		const wrong = "// TypeScript Version: 3.14";
-		assert.throws(() => parseTypeScriptVersionLine(wrong));
-	});
+        const wrong = "// TypeScript Version: 3.14";
+        assert.throws(() => parseTypeScriptVersionLine(wrong));
+    });
 
-	it("allows typescript 2.3", () => {
-		const src = "// TypeScript Version: 2.3";
-		assert.equal(parseTypeScriptVersionLine(src), "2.3");
-	});
-	
-	it("allows post 3 version tags", () => {
-		const src = "// TypeScript Version: 3.0";
-		assert.equal(parseTypeScriptVersionLine(src), "3.0")
-	})
+    it("allows typescript 2.3", () => {
+        const src = "// TypeScript Version: 2.3";
+        assert.equal(parseTypeScriptVersionLine(src), "2.3");
+    });
 
-	it("does not allow unallowed version tags", () => {
-		const src = "// TypeScript Version: 4.7";
-		assert.throws(() => parseTypeScriptVersionLine(src), `Could not parse version: line is ${src}`);
-	})
+    it("allows post 3 version tags", () => {
+        const src = "// TypeScript Version: 3.0";
+        assert.equal(parseTypeScriptVersionLine(src), "3.0")
+    })
+
+    it("does not allow unallowed version tags", () => {
+        const src = "// TypeScript Version: 4.7";
+        assert.throws(() => parseTypeScriptVersionLine(src), `Could not parse version: line is ${src}`);
+    })
 });
 
 describe("tagsToUpdate", () => {
-	it("works", () => {
-		assert.deepEqual(
-			TypeScriptVersion.tagsToUpdate("2.5"),
-			["ts2.5", "ts2.6", "ts2.7", "ts2.8", "ts2.9", "ts3.0", "ts3.1", "ts3.2", "ts3.3", "ts3.4", "latest"]);
-	});
+    it("works", () => {
+        assert.deepEqual(
+            TypeScriptVersion.tagsToUpdate("2.5"),
+            ["ts2.5", "ts2.6", "ts2.7", "ts2.8", "ts2.9", "ts3.0", "ts3.1", "ts3.2", "ts3.3", "ts3.4", "ts3.5", "latest"]);
+    });
 });
 
 function dedent(strings: TemplateStringsArray): string {
-	assert.equal(strings.length, 1);
-	const x = strings[0].trim();
-	return x.replace(/\n\t\t\t/g, "\n");
+    assert.equal(strings.length, 1);
+    const x = strings[0].trim();
+    return x.replace(/\n +/g, "\n");
 }
