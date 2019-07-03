e51c77ff90a1abb5ba28a5f659747418b255cd88
Andrew Branch <andrew@wheream.io>
2019-07-03 09:45:56 -0700

    Clean after switching branches

--- packages/definitely-not-slow/src/cli/compare.ts
+++ packages/definitely-not-slow/src/cli/compare.ts
@@ -116,7 +116,7 @@ export async function compareBenchmarks({
     }
     if (needsRerun) {
       console.log(`No previous benchmark for ${packageName}/v${packageVersion}. Checking out master and running one...`);
-      await execAndThrowErrors('git checkout origin/master', definitelyTypedPath);
+      await execAndThrowErrors('git checkout origin/master && git clean -df', definitelyTypedPath);
       const latest = await benchmarkPackage(packageName, packageVersion.toString(), new Date(), {
         definitelyTypedPath,
         printSummary: false,
@@ -129,7 +129,7 @@ export async function compareBenchmarks({
         installTypeScript: false,
         maxRunSeconds,
       });
-      await execAndThrowErrors(`git checkout -`, definitelyTypedPath);
+      await execAndThrowErrors(`git checkout - && git clean -df`, definitelyTypedPath);
       latestBenchmark = latest && latest.summary;
     }
   }
