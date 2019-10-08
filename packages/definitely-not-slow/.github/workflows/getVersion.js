1b862ff45994e932e8a2518f56750f92c115ca8e
Andrew Branch <andrew@wheream.io>
2019-10-08 15:05:54 -0700

    Fix version string

--- packages/definitely-not-slow/.github/workflows/getVersion.js
+++ packages/definitely-not-slow/.github/workflows/getVersion.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
 
+/**
+ * @param {string | number} value
+ * @param {number} length
+ */
+function padInt(value, length) {
+  value = value.toString();
+  while (value.length < length) {
+    value = "0" + value;
+  }
+  return value;
+}
+
 const packageJson = require('../../package.json');
 const version = packageJson.version;
 const date = new Date();
 const timestamp = [
   date.getUTCFullYear(),
-  date.getUTCMonth() + 1,
-  date.getUTCDate(),
-  date.getUTCHours(),
-  date.getUTCMinutes(),
+  padInt(date.getUTCMonth() + 1, 2),
+  padInt(date.getUTCDate(), 2),
+  padInt(date.getUTCHours(), 2),
+  padInt(date.getUTCMinutes(), 2),
 ].join('');
 
 console.log(`${version}-alpha.${timestamp}`);
