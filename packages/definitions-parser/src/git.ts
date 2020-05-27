dde283840d68c589b360b5687e2c521985f486d4
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-05-27 11:14:28 -0700

    swap get/checkNotNeedPackage order to make more sense

--- packages/definitions-parser/src/git.ts
+++ packages/definitions-parser/src/git.ts
@@ -133,6 +133,32 @@ export async function getAffectedPackagesFromDiff(
 }
 
 /**
+ * 1. find all the deleted files and group by toplevel
+ * 2. Make sure that there are no packages left with deleted entries
+ * 3. make sure that each toplevel deleted has a matching entry in notNeededPackages
+ */
+export function getNotNeededPackages(allPackages: AllPackages, diffs: GitDiff[]): Iterable<NotNeededPackage> {
+  const deletedPackages = new Set(
+    diffs
+      .filter(d => d.status === "D")
+      .map(
+        d =>
+          assertDefined(
+            getDependencyFromFile(d.file),
+            `Unexpected file deleted: ${d.file}
+When removing packages, you should only delete files that are a part of removed packages.`
+          ).name
+      )
+  );
+  return mapIterable(deletedPackages, p => {
+    if (allPackages.hasTypingFor({ name: p, version: "*" })) {
+      throw new Error(`Please delete all files in ${p} when adding it to notNeededPackages.json.`);
+    }
+    return assertDefined(allPackages.getNotNeededPackage(p), `Deleted package ${p} is not in notNeededPackages.json.`);
+  });
+}
+
+/**
  * 1. libraryName must exist on npm (SKIPPED and preferably/optionally have been the libraryName in just-deleted header)
  * (SKIPPED 2.) sourceRepoURL must exist and be the npm homepage
  * 3. asOfVersion must be newer than `@types/name@latest` on npm
@@ -167,30 +193,4 @@ it is supposed to replace, ${latestTypings.versionString} of ${unneeded.fullNpmN
     source.versions.has(unneeded.version.versionString),
     `The specified version ${unneeded.version.versionString} of ${unneeded.libraryName} is not on npm.`
   );
-}
-
-/**
- * 1. find all the deleted files and group by toplevel
- * 2. Make sure that there are no packages left with deleted entries
- * 3. make sure that each toplevel deleted has a matching entry in notNeededPackages
- */
-export function getNotNeededPackages(allPackages: AllPackages, diffs: GitDiff[]): Iterable<NotNeededPackage> {
-  const deletedPackages = new Set(
-    diffs
-      .filter(d => d.status === "D")
-      .map(
-        d =>
-          assertDefined(
-            getDependencyFromFile(d.file),
-            `Unexpected file deleted: ${d.file}
-When removing packages, you should only delete files that are a part of removed packages.`
-          ).name
-      )
-  );
-  return mapIterable(deletedPackages, p => {
-    if (allPackages.hasTypingFor({ name: p, version: "*" })) {
-      throw new Error(`Please delete all files in ${p} when adding it to notNeededPackages.json.`);
-    }
-    return assertDefined(allPackages.getNotNeededPackage(p), `Deleted package ${p} is not in notNeededPackages.json.`);
-  });
 }
