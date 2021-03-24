5d7f61a0feffe0983d660ffb4916bb20c777f2ca
Andrew Branch <andrew@wheream.io>
2021-03-24 09:33:10 -0700

    Make test HTTP trigger run the whole thing

--- packages/publisher/src/functions/HttpTrigger.ts
+++ packages/publisher/src/functions/HttpTrigger.ts
@@ -1,13 +1,9 @@
 import { AzureFunction, Context } from "@azure/functions";
-import { existsSync, mkdirp } from "fs-extra";
+import main from "../main";
 
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-
-    context.log("existsSync", existsSync("/storage"));
-    context.log("mkdirp");
-    await mkdirp("/storage/DefinitelyTyped");
-    context.log("existsSync (subfolder)", existsSync("/storage/DefinitelyTyped"));
+  context.log('HTTP trigger function processed a request.');
+  return main();
 };
 
 export default httpTrigger;
