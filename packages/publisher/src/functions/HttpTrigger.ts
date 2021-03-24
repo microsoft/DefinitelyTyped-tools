ba740b798f7c4b65ca0cfada90db8d82348d6724
Andrew Branch <andrew@wheream.io>
2021-03-23 17:30:37 -0700

    Add test function for fileshare

---
+++ packages/publisher/src/functions/HttpTrigger.ts
@@ -0,0 +1,13 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { existsSync, mkdirp } from "fs-extra";
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    context.log("existsSync", existsSync("/storage"));
+    context.log("mkdirp");
+    await mkdirp("/storage/DefinitelyTyped");
+    context.log("existsSync (subfolder)", existsSync("/storage/DefinitelyTyped"));
+};
+
+export default httpTrigger;
