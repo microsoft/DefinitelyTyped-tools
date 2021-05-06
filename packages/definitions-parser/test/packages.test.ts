cdfc9ae382e95a36cb989690ccd90b2c27375b26
James Garbutt <43081j@users.noreply.github.com>
2021-05-06 18:42:34 +0100

    add tests for typingsdata (#228)
    * add tests for typingsdata
    
    * add tests for notneeded
    
    * a few more

--- packages/definitions-parser/test/packages.test.ts
+++ packages/definitions-parser/test/packages.test.ts
@@ -1,8 +1,19 @@
 import { createMockDT } from "../src/mocks";
 import { getTypingInfo } from "../src/lib/definition-parser";
-import { AllPackages, TypingsVersions } from "../src/packages";
+import {
+  AllPackages,
+  TypingsVersions,
+  TypingsData,
+  License,
+  getMangledNameForScopedPackage,
+  NotNeededPackage,
+  getLicenseFromPackageJson,
+  getDependencyFromFile
+} from "../src/packages";
 import { parseDefinitions } from "../src/parse-definitions";
 import { quietLoggerWithErrors } from "@definitelytyped/utils";
+import { createTypingsVersionRaw } from "./utils";
+import { TypeScriptVersion } from "@definitelytyped/typescript-versions";
 
 describe(AllPackages, () => {
   let allPackages: AllPackages;
@@ -19,6 +30,31 @@ describe(AllPackages, () => {
     expect(Array.from(allPackages.allDependencyTypings(pkg), ({ id }) => id)).toEqual([
       { name: "jquery", version: { major: 1, minor: 0 } }
     ]);
+  });
+
+  describe("getNotNeededPackage", () => {
+    it("returns specified package", () => {
+      const pkg = allPackages.getNotNeededPackage("angular");
+      expect(pkg).toBeTruthy();
+      expect(allPackages.getNotNeededPackage("non-existent")).toBe(undefined);
+    });
+  });
+
+  describe("hasTypingFor", () => {
+    it("returns true if typings exist", () => {
+      expect(
+        allPackages.hasTypingFor({
+          name: "jquery",
+          version: "*"
+        })
+      ).toBe(true);
+      expect(
+        allPackages.hasTypingFor({
+          name: "nonExistent",
+          version: "*"
+        })
+      ).toBe(false);
+    });
   });
 });
 
@@ -63,5 +99,191 @@ describe(TypingsVersions, () => {
   it("formats missing version error nicely", () => {
     expect(() => versions.get({ major: 111, minor: 1001 })).toThrow("Could not find version 111.1001");
     expect(() => versions.get({ major: 111 })).toThrow("Could not find version 111.*");
+  });
+});
+
+describe(TypingsData, () => {
+  let data: TypingsData;
+
+  beforeEach(() => {
+    const versions = createTypingsVersionRaw(
+      "known",
+      {
+        "dependency-1": "*"
+      },
+      [],
+      {}
+    );
+    data = new TypingsData(versions["1.0"], true);
+  });
+
+  it("sets the correct properties", () => {
+    expect(data.name).toBe("known");
+    expect(data.testDependencies).toEqual([]);
+    expect(data.contributors).toEqual([
+      {
+        name: "Bender",
+        url: "futurama.com",
+        githubUsername: "bender"
+      }
+    ]);
+    expect(data.major).toBe(1);
+    expect(data.minor).toBe(0);
+    expect(data.minTypeScriptVersion).toBe(TypeScriptVersion.lowest);
+    expect(data.typesVersions).toEqual([]);
+    expect(data.files).toEqual(["index.d.ts"]);
+    expect(data.license).toBe(License.MIT);
+    expect(data.packageJsonDependencies).toEqual([]);
+    expect(data.contentHash).toBe("11111111111111");
+    expect(data.declaredModules).toEqual([]);
+    expect(data.projectName).toBe("zombo.com");
+    expect(data.globals).toEqual([]);
+    expect(data.pathMappings).toEqual({});
+    expect(data.dependencies).toEqual({
+      "dependency-1": "*"
+    });
+    expect(data.id).toEqual({
+      name: "known",
+      version: {
+        major: 1,
+        minor: 0
+      }
+    });
+    expect(data.isNotNeeded()).toBe(false);
+  });
+
+  describe("unescapedName", () => {
+    it("returns the name when unscoped", () => {
+      expect(data.unescapedName).toBe("known");
+    });
+
+    it("returns scoped names correctly", () => {
+      const versions = createTypingsVersionRaw("foo__bar", {}, [], {});
+      data = new TypingsData(versions["1.0"], true);
+
+      expect(data.unescapedName).toBe("@foo/bar");
+    });
+  });
+
+  describe("desc", () => {
+    it("returns the name if latest version", () => {
+      expect(data.desc).toBe("known");
+    });
+
+    it("returns the verioned name if not latest", () => {
+      const versions = createTypingsVersionRaw("known", {}, [], {});
+      data = new TypingsData(versions["1.0"], false);
+
+      expect(data.desc).toBe("known v1.0");
+    });
+  });
+
+  describe("fullNpmName", () => {
+    it("returns scoped name", () => {
+      expect(data.fullNpmName).toBe("@types/known");
+    });
+
+    it("returns mangled name if scoped", () => {
+      const versions = createTypingsVersionRaw("@foo/bar", {}, [], {});
+      data = new TypingsData(versions["1.0"], false);
+
+      expect(data.fullNpmName).toBe("@types/foo__bar");
+    });
+  });
+
+  describe("fullEscapedNpmName", () => {
+    it("returns escaped name", () => {
+      expect(data.fullEscapedNpmName).toBe("@types%2fknown");
+    });
+  });
+});
+
+describe(getMangledNameForScopedPackage, () => {
+  it("returns unscoped names as-is", () => {
+    expect(getMangledNameForScopedPackage("foo")).toBe("foo");
+  });
+
+  it("returns mangled names for scoped packages", () => {
+    expect(getMangledNameForScopedPackage("@foo/bar")).toBe("foo__bar");
+  });
+});
+
+describe(NotNeededPackage, () => {
+  let data: NotNeededPackage;
+
+  beforeEach(() => {
+    data = new NotNeededPackage("types-package", "real-package", "1.0.0");
+  });
+
+  it("sets the correct properties", () => {
+    expect(data.license).toBe(License.MIT);
+    expect(data.name).toBe("types-package");
+    expect(data.libraryName).toBe("real-package");
+    expect(data.version).toEqual({
+      major: 1,
+      minor: 0,
+      patch: 0
+    });
+    expect(data.major).toBe(1);
+    expect(data.minor).toBe(0);
+    expect(data.isLatest).toBe(true);
+    expect(data.isNotNeeded()).toBe(true);
+    expect(data.declaredModules).toEqual([]);
+    expect(data.minTypeScriptVersion).toBe(TypeScriptVersion.lowest);
+    expect(data.deprecatedMessage()).toBe(
+      "This is a stub types definition. real-package provides its own type definitions, so you do not need this installed."
+    );
+  });
+});
+
+describe(getLicenseFromPackageJson, () => {
+  it("returns MIT by default", () => {
+    expect(getLicenseFromPackageJson(undefined)).toBe(License.MIT);
+  });
+
+  it("throws if license is MIT", () => {
+    expect(() => getLicenseFromPackageJson("MIT")).toThrow();
+  });
+
+  it("returns known licenses", () => {
+    expect(getLicenseFromPackageJson(License.Apache20)).toBe(License.Apache20);
+  });
+
+  it("throws if unknown license", () => {
+    expect(() => getLicenseFromPackageJson("nonsense")).toThrow();
+  });
+});
+
+describe(getDependencyFromFile, () => {
+  it("returns undefined for unversioned paths", () => {
+    expect(getDependencyFromFile("types/a")).toBe(undefined);
+  });
+
+  it("returns undefined if not in types directory", () => {
+    expect(getDependencyFromFile("foo/bar/v3/baz")).toBe(undefined);
+  });
+
+  it("returns parsed version for versioned paths", () => {
+    expect(getDependencyFromFile("types/a/v3.5")).toEqual({
+      name: "a",
+      version: {
+        major: 3,
+        minor: 5
+      }
+    });
+    expect(getDependencyFromFile("types/a/v3")).toEqual({
+      name: "a",
+      version: {
+        major: 3,
+        minor: undefined
+      }
+    });
+  });
+
+  it("returns undefined for unversioned subpaths", () => {
+    expect(getDependencyFromFile("types/a/vnotaversion")).toEqual({
+      name: "a",
+      version: "*"
+    });
   });
 });
