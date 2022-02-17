microsoft/DefinitelyTyped-tools

commit 6a17c0d7dadf334a2bf345c51607ff29b653144b

Fix self-dependency versions (#403)
* Fix self-dependency versions

* Print offending sections of yarn.lock

* Remove test input from yarn.lock

* Spacing

* Spacing

* Spacing

* Simplify bash

Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

Co-authored-by: Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>

packages/dtslint/src/index.ts
@@ -20,6 +20,8 @@ async function main(): Promise<void> {
   let lookingForTsLocal = false;
   let tsLocal: string | undefined;
 
+  console.log(`dtslint@${require("../package.json").version}`);
+
   for (const arg of args) {
     if (lookingForTsLocal) {
       if (arg.startsWith("--")) {

packages/dtslint-runner/src/index.ts
@@ -95,6 +95,7 @@ if (!module.parent) {
   };
 
   logUncaughtErrors(async () => {
+    console.log(`dtslint-runner@${require("../package.json").version}`);
     const failures = await runDTSLint(options);
     process.exit(failures);
   });

.github/workflows/ci.yml
@@ -17,6 +17,19 @@ jobs:
     - uses: actions/setup-node@v2
       with:
         node-version: ${{ matrix.node }}
+    - name: check dependencies
+      run: |
+        if grep "@definitelytyped/" yarn.lock; then
+          echo ""
+          echo "A package in this monorepo is depending on an older version of one of its siblings. \
+        Ensure all '@definitelytyped/*' dependencies in package.json files use the version \
+        listed in lerna.json to let lerna automatically keep them in sync."
+
+          echo ""
+          echo "yarn.lock"
+          cat yarn.lock | grep -A 5 -n "@definitelytyped/"
+          exit 1
+        fi
     - name: install
       run: yarn install --frozen-lockfile
     - name: lint

commit e8b3dc646c3f62f6a4d99d0327277a481b77b511

Fix incorrect reference to package.json
This is causing DefinitelyTyped CI builds to fail.

This seems to have been unintentionally been broken in https://github.com/microsoft/DefinitelyTyped-tools/pull/403

packages/dtslint/src/index.ts
@@ -20,7 +20,7 @@ async function main(): Promise<void> {
   let lookingForTsLocal = false;
   let tsLocal: string | undefined;
 
-  console.log(`dtslint@${require("../package.json").version}`);
+  console.log(`dtslint@${require("../../package.json").version}`);
 
   for (const arg of args) {
     if (lookingForTsLocal) {