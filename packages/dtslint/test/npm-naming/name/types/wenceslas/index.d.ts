7fa23beef412543dc196475a538486462e4ed920
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-03-22 15:57:51 -0700

    Change non-existent package name in tests
    parseltongue now exists, so change to wenceslas.

--- packages/dtslint/test/npm-naming/name/types/wenceslas/index.d.ts
+++ packages/dtslint/test/npm-naming/name/types/wenceslas/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for package parseltongue 1.0
+// Type definitions for package wenceslas 1.0
 // Project: https://github.com/bobby-headers/dt-header
 // Definitions by: Jane Doe <https://github.com/janedoe>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
