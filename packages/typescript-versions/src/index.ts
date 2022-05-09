3d432b2cd4581a6fb8005b303e5c615eef1c6714
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-05-09 08:00:26 -0700

    Add TS4.8 to supported versions (#454)
    For use after typescript's main branch switches to 4.8 at the start of
    the 4.7 RC period.

--- packages/typescript-versions/src/index.ts
+++ packages/typescript-versions/src/index.ts
@@ -50,7 +50,7 @@ export type UnsupportedTypeScriptVersion =
  * Parseable and supported TypeScript versions.
  * Only add to this list if we will support this version on DefinitelyTyped.
  */
-export type TypeScriptVersion = "3.9" | "4.0" | "4.1" | "4.2" | "4.3" | "4.4" | "4.5" | "4.6" | "4.7";
+export type TypeScriptVersion = "3.9" | "4.0" | "4.1" | "4.2" | "4.3" | "4.4" | "4.5" | "4.6" | "4.7" | "4.8";
 
 export type AllTypeScriptVersion = UnsupportedTypeScriptVersion | TypeScriptVersion;
 
@@ -58,7 +58,7 @@ export namespace TypeScriptVersion {
   /** Add to this list when a version actually ships.  */
   export const shipped: readonly TypeScriptVersion[] = ["3.9", "4.0", "4.1", "4.2", "4.3", "4.4", "4.5", "4.6"];
   /** Add to this list when a version is available as typescript@next */
-  export const supported: readonly TypeScriptVersion[] = [...shipped, "4.7"];
+  export const supported: readonly TypeScriptVersion[] = [...shipped, "4.7", "4.8"];
   /** Add to this list when it will no longer be supported on Definitely Typed */
   export const unsupported: readonly UnsupportedTypeScriptVersion[] = [
     "2.0",
