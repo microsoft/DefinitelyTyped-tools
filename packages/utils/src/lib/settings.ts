3f2f642501964ddbe250e5f0cbb0e6dc51549bee
Andrew Branch <andrewbranch@users.noreply.github.com>
2022-06-09 12:46:09 -0700

    Fix cache dir path (#478)

--- packages/utils/src/lib/settings.ts
+++ packages/utils/src/lib/settings.ts
@@ -2,3 +2,4 @@ import { joinPaths } from "../fs";
 const root = joinPaths(__dirname, "..", "..");
 const storageDirPath = process.env.STORAGE_DIR || root;
 export const logDir = joinPaths(storageDirPath, "logs");
+export const cacheDirPath = joinPaths(storageDirPath, "cache");
