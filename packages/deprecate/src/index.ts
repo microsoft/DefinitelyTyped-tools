dc9524e5263d08c4f2f602ab98e6361aab90eedf
Jack Bates <jack@nottheoilrig.com>
2022-07-08 16:12:59 -0700

    Port from libnpmsearch -> all-the-package-names

--- packages/deprecate/src/index.ts
+++ packages/deprecate/src/index.ts
@@ -3,9 +3,10 @@
 import console from "console";
 import process from "process";
 import { AllPackages, getDefinitelyTyped } from "@definitelytyped/definitions-parser";
-import { NpmPublishClient } from "@definitelytyped/utils";
+import { NpmPublishClient, cacheDir } from "@definitelytyped/utils";
 import { graphql } from "@octokit/graphql";
-import search from "libnpmsearch";
+import names from "all-the-package-names" assert { type: "json" };
+import pacote from "pacote";
 // @ts-expect-error
 import { packages } from "typescript-dom-lib-generator/deploy/createTypesPackages.js";
 import yargs from "yargs";
@@ -19,29 +20,29 @@ import yargs from "yargs";
   const allPackages = await AllPackages.read(dt);
   const client = await NpmPublishClient.create(process.env.NPM_TOKEN!);
   // Loop over npm @types packages and mark as deprecated any that no longer exist in the DT repo.
-  let from = 0;
-  let results;
-  do {
-    const opts = { limit: 250, from };
-    // Won't return already-deprecated packages.
-    results = await search("@types", opts);
-    for (const result of results) {
-      // Skip @types/web, etc.
-      if (domLibs.has(result.name)) continue;
-      const types = result.name.slice("@types/".length);
-      // Skip ones that exist, either in the types/ directory or in notNeededPackages.json.
-      if (allPackages.tryGetLatestVersion(types) || allPackages.getNotNeededPackage(types)) continue;
-      const msg = await fetchMsg(types);
-      if (!msg) {
-        console.log(`Could not find the commit that removed types/${types}/.`);
-        continue;
-      }
-      console.log(`Deprecating ${result.name}: ${msg}`);
-      if (!dryRun) await client.deprecate(result.name, "*", msg);
+  for (const name of names) {
+    // Skip @types/web, etc.
+    if (!name.startsWith("@types/") || domLibs.has(name)) continue;
+    const types = name.slice("@types/".length);
+    // Skip ones that exist, either in the types/ directory or in notNeededPackages.json.
+    if (allPackages.tryGetLatestVersion(types) || allPackages.getNotNeededPackage(types)) continue;
+    // Skip already-deprecated packages.
+    // Cache package deprecation indefinitely.
+    const offline = await pacote.manifest(name, { cache: cacheDir, offline: true }).catch((reason) => {
+      if (reason.code !== "ENOTCACHED") throw reason;
+      return undefined;
+    });
+    if (offline?.deprecated) continue;
+    const online = await pacote.manifest(name, { cache: cacheDir, preferOnline: true });
+    if (online.deprecated) continue;
+    const msg = await fetchMsg(types);
+    if (!msg) {
+      console.log(`Could not find the commit that removed types/${types}/.`);
+      continue;
     }
-    from += results.length;
-    // The registry API clamps limit at 250 and from at 5,000, so we can only loop over 5,250 packages, for now.
-  } while (results.length >= 250 && from <= 5000);
+    console.log(`Deprecating ${name}: ${msg}`);
+    if (!dryRun) await client.deprecate(name, "*", msg);
+  }
 })();
 
 /** Reference the commit/PR that removed the named types. */
