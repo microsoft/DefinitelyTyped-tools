109bff57da2d8317876eef8c9291e222618b3c0a
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-09-22 15:54:57 -0700

    Special case node's typeVersions for 4.9 beta (#535)
    * Special case node's typeVersions for 4.9 beta
    
    Require a ">4.9.0-a" entry to handle 4.9 beta and dev releases.
    
    * yarn format

--- packages/dtslint/src/checks.ts
+++ packages/dtslint/src/checks.ts
@@ -25,7 +25,10 @@ export async function checkPackageJson(dirPath: string, typesVersions: readonly 
 
   if (needsTypesVersions) {
     assert.strictEqual((pkgJson as any).types, "index", `"types" in '${pkgJsonPath}' should be "index".`);
-    const expected = makeTypesVersionsForPackageJson(typesVersions);
+    const expected = makeTypesVersionsForPackageJson(typesVersions) as Record<string, object>;
+    if (dirPath.endsWith("/node") || dirPath.endsWith("/node/v14") || dirPath.endsWith("/node/v16")) {
+      expected[">4.9.0-a"] = { "*": ["*"] };
+    }
     assert.deepEqual(
       (pkgJson as any).typesVersions,
       expected,
