21b2569aedcb117f76d107574616fad4674f7e68
Josh Goldberg <git@joshuakgoldberg.com>
2022-09-26 12:44:27 -0400

    Converted prefer-declare-function from TSLint to ESLint

---
+++ packages/dtslint/src/rules/prefer-declare-function.ts
@@ -0,0 +1,34 @@
+import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/utils";
+
+import { createRule } from "../util";
+
+const rule = createRule({
+  defaultOptions: [],
+  meta: {
+    docs: {
+      description: "Forbids `export const x = () => void`.",
+      recommended: "error",
+    },
+    messages: {
+      variableFunction: "Use a function declaration instead of a variable of function type.",
+    },
+    schema: [],
+    type: "problem",
+  },
+  name: "prefer-declare-function",
+  create(context) {
+    return {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      "ExportNamedDeclaration > VariableDeclaration > VariableDeclarator"(node: TSESTree.VariableDeclarator) {
+        if (node.id.typeAnnotation?.typeAnnotation.type === AST_NODE_TYPES.TSFunctionType) {
+          context.report({
+            messageId: "variableFunction",
+            node: node.id,
+          });
+        }
+      },
+    };
+  },
+});
+
+export = rule;
---
+++ packages/dtslint/test/prefer-declare-function.test.ts
@@ -0,0 +1,39 @@
+import { ESLintUtils } from "@typescript-eslint/utils";
+
+import * as noConstEnum from "../src/rules/prefer-declare-function";
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+});
+
+ruleTester.run("prefer-declare-function", noConstEnum, {
+  invalid: [
+    {
+      code: `export const example: () => void;`,
+      errors: [
+        {
+          column: 14,
+          endColumn: 33,
+          line: 1,
+          messageId: "variableFunction",
+        },
+      ],
+    },
+    {
+      code: `
+        namespace N {
+            export const example: () => void;
+        }
+`,
+      errors: [
+        {
+          column: 26,
+          endColumn: 45,
+          line: 3,
+          messageId: "variableFunction",
+        },
+      ],
+    },
+  ],
+  valid: [`function example(): void`],
+});
