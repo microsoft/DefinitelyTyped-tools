cb0179da5d4f16cc9c947415dcb6c2c8253f6158
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-02-16 14:23:49 -0800

    fix semicolon lint

--- packages/dtslint/src/lint.ts
+++ packages/dtslint/src/lint.ts
@@ -22,7 +22,7 @@ export async function lint(
 ): Promise<string | undefined> {
   const tsconfigPath = joinPaths(dirPath, "tsconfig.json");
   const estree = await import(require.resolve("@typescript-eslint/typescript-estree", { paths: [dirPath] }));
-  process.env.TSESTREE_SINGLE_RUN = 'true'
+  process.env.TSESTREE_SINGLE_RUN = "true";
   // TODO: To remove tslint, replace this with a ts.createProgram (probably)
   const lintProgram = Linter.createProgram(tsconfigPath);
 
