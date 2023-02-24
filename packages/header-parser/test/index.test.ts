563a2dd2f8034a703e5cdf27b1606472e1be0e71
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-02-24 15:49:53 -0800

    Add TS 5.1 in preparation for 5.0 RC

--- packages/header-parser/test/index.test.ts
+++ packages/header-parser/test/index.test.ts
@@ -169,7 +169,7 @@ describe("isTypeScriptVersion", () => {
 
 describe("range", () => {
   it("works", () => {
-    expect(TypeScriptVersion.range("4.2")).toEqual(["4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "5.0"]);
+    expect(TypeScriptVersion.range("4.2")).toEqual(["4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "5.0", "5.1"]);
   });
   it("includes 4.2 onwards", () => {
     expect(TypeScriptVersion.range("4.2")).toEqual(TypeScriptVersion.supported);
@@ -188,6 +188,7 @@ describe("tagsToUpdate", () => {
       "ts4.8",
       "ts4.9",
       "ts5.0",
+      "ts5.1",
       "latest",
     ]);
   });
--- packages/typescript-versions/src/index.ts
+++ packages/typescript-versions/src/index.ts
@@ -55,7 +55,7 @@ export type UnsupportedTypeScriptVersion =
  * Parseable and supported TypeScript versions.
  * Only add to this list if we will support this version on Definitely Typed.
  */
-export type TypeScriptVersion = "4.2" | "4.3" | "4.4" | "4.5" | "4.6" | "4.7" | "4.8" | "4.9" | "5.0";
+export type TypeScriptVersion = "4.2" | "4.3" | "4.4" | "4.5" | "4.6" | "4.7" | "4.8" | "4.9" | "5.0" | "5.1";
 
 export type AllTypeScriptVersion = UnsupportedTypeScriptVersion | TypeScriptVersion;
 
@@ -63,7 +63,7 @@ export namespace TypeScriptVersion {
   /** Add to this list when a version actually ships.  */
   export const shipped: readonly TypeScriptVersion[] = ["4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9"];
   /** Add to this list when a version is available as typescript@next */
-  export const supported: readonly TypeScriptVersion[] = [...shipped, "5.0"];
+  export const supported: readonly TypeScriptVersion[] = [...shipped, "5.0", "5.1"];
   /** Add to this list when it will no longer be supported on Definitely Typed */
   export const unsupported: readonly UnsupportedTypeScriptVersion[] = [
     "2.0",
