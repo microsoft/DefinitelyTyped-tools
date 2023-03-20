7e7c7b77590acf8f5155301d0e365c6c439b4452
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-03-20 14:15:26 -0700

    First draft
    It passes some basic tests!

---
+++ packages/dtslint/test/redundant-undefined.test.ts
@@ -0,0 +1,39 @@
+import { ESLintUtils } from "@typescript-eslint/utils";
+
+import * as redundantUndefined from "../src/rules/redundant-undefined";
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+});
+
+ruleTester.run("redundant-undefined", redundantUndefined, {
+  invalid: [
+    {
+
+      code: `function f(s?: string | undefined): void {}`,
+      errors: [
+        {
+          line: 1,
+          messageId: "redundantUndefined",
+        },
+      ],
+    },
+    {
+
+      code: `function f([a, b]?: [number, number] | undefined): void {}`,
+      errors: [
+        {
+          line: 1,
+          messageId: "redundantUndefined",
+        },
+      ],
+    },
+  ],
+  valid: [`
+interface I {
+    ok?: string | undefined;
+    s?: string;
+    almost?: number | string;
+}
+`],
+});
