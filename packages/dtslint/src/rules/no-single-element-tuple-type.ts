9cacb5e854cbf2eeb072e9a0815071853c6d0702
Josh Goldberg <git@joshuakgoldberg.com>
2023-03-24 11:42:31 -0400

    Converted no-single-element-tuple-type from TSLint to ESLint

---
+++ packages/dtslint/src/rules/no-single-element-tuple-type.ts
@@ -0,0 +1,31 @@
+import { createRule } from "../util";
+import { TSESTree } from "@typescript-eslint/utils";
+
+const rule = createRule({
+  name: "no-single-element-tuple-type",
+  defaultOptions: [],
+  meta: {
+    type: "problem",
+    docs: {
+      description: "Forbids `[T]`, which should be `T[]`.",
+      recommended: "error",
+    },
+    messages: {
+      singleElementTupleType: `Type [T] is a single-element tuple type. You probably meant T[].`,
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      "TSTupleType[elementTypes.length=1]"(node: TSESTree.TSTupleType) {
+        context.report({
+          messageId: "singleElementTupleType",
+          node,
+        });
+      },
+    };
+  },
+});
+
+export = rule;
---
+++ packages/dtslint/test/no-single-element-tuple-type.test.ts
@@ -0,0 +1,28 @@
+import { ESLintUtils } from "@typescript-eslint/utils";
+
+import * as noDeclareCurrentPackage from "../src/rules/no-single-element-tuple-type";
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: "@typescript-eslint/parser",
+});
+
+ruleTester.run("no-single-element-tuple-type", noDeclareCurrentPackage, {
+  invalid: [
+    {
+      code: `type Test<T> = [T];`,
+      errors: [
+        {
+          line: 1,
+          messageId: "singleElementTupleType",
+        },
+      ],
+    },
+  ],
+  valid: [
+    `type Test = number[];`,
+    `type Test<T> = T;`,
+    `type Test<T> = T[];`,
+    `type Test<T> = [T, number];`,
+    `type Test<T> = [T, T];`,
+  ],
+});
