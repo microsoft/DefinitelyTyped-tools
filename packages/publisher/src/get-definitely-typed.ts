f2b8bf59395ae70820102c4c3b2d0b833b1acb5f
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-05-11 08:46:58 -0700

    Revert appinsights removal (#674)
    * Revert "Remove applicationinsights (#673)"
    
    This reverts commit 46828d62abf930118a161f63d903af737119e7da.
    
    * re-remove just the latency logging

--- packages/publisher/src/get-definitely-typed.ts
+++ packages/publisher/src/get-definitely-typed.ts
@@ -1,10 +1,15 @@
 import assert from "assert";
 import yargs from "yargs";
+import applicationinsights from "applicationinsights";
 import { logUncaughtErrors, loggerWithErrors } from "@definitelytyped/utils";
 import { getDefinitelyTyped } from "@definitelytyped/definitions-parser";
 import { defaultLocalOptions, defaultRemoteOptions } from "./lib/common";
 
 if (!module.parent) {
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    applicationinsights.setup();
+    applicationinsights.start();
+  }
   const dry = !!yargs.argv.dry;
   console.log("gettingDefinitelyTyped: " + (dry ? "from github" : "locally"));
   logUncaughtErrors(async () => {
