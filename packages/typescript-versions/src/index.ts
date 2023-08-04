ff78f8db5f31932d87cbb28b5e7178b4080046aa
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-08-04 16:11:59 -0700

    Revert "Support 5.3 (#703)"
    This reverts commit 14b335737264df281d2f9e74e6196d0b58f83fe8.

--- packages/typescript-versions/src/index.ts
+++ packages/typescript-versions/src/index.ts
@@ -57,7 +57,7 @@ export type UnsupportedTypeScriptVersion =
  * Parseable and supported TypeScript versions.
  * Only add to this list if we will support this version on Definitely Typed.
  */
-export type TypeScriptVersion = "4.4" | "4.5" | "4.6" | "4.7" | "4.8" | "4.9" | "5.0" | "5.1" | "5.2" | "5.3";
+export type TypeScriptVersion = "4.4" | "4.5" | "4.6" | "4.7" | "4.8" | "4.9" | "5.0" | "5.1" | "5.2";
 
 export type AllTypeScriptVersion = UnsupportedTypeScriptVersion | TypeScriptVersion;
 
@@ -65,7 +65,7 @@ export namespace TypeScriptVersion {
   /** Add to this list when a version actually ships.  */
   export const shipped: readonly TypeScriptVersion[] = ["4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "5.0", "5.1"];
   /** Add to this list when a version is available as typescript@next */
-  export const supported: readonly TypeScriptVersion[] = [...shipped, "5.2", "5.3"];
+  export const supported: readonly TypeScriptVersion[] = [...shipped, "5.2"];
   /** Add to this list when it will no longer be supported on Definitely Typed */
   export const unsupported: readonly UnsupportedTypeScriptVersion[] = [
     "2.0",
