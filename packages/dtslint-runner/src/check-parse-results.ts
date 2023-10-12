53402d23fc4a71994e63abddcaf9674e33d64669
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-10-12 09:19:55 -0700

    forbid pinning on-DT @types/ packages

--- packages/dtslint-runner/src/check-parse-results.ts
+++ packages/dtslint-runner/src/check-parse-results.ts
@@ -1,5 +1,7 @@
 import console from "console";
 import { AllPackages, getDefinitelyTyped } from "@definitelytyped/definitions-parser";
+import { assertDefined } from "@definitelytyped/utils";
+import * as semver from "semver"
 if (require.main === module) {
   const options = { definitelyTypedPath: undefined, progress: false, parseInParallel: false };
   getDefinitelyTyped(options, console).then((dt) => {
@@ -11,6 +13,13 @@ export function checkParseResults(allPackages: AllPackages): string[] {
   const errors = [];
   for (const pkg of allPackages.allTypings()) {
     for (const dep of allPackages.allDependencyTypings(pkg)) {
+      // check raw version because parsed version doesn't currently retain range information
+      const version = assertDefined(new Map(pkg.allPackageJsonDependencies()).get(dep.name));
+      if (semver.parse(version)) {
+        errors.push(
+          `${pkg.desc}'s dependency on ${dep.desc}@${version} must use "*" or a version range, not a specific version.`
+        );
+      }
       if (dep.minTypeScriptVersion > pkg.minTypeScriptVersion) {
         errors.push(
           `${pkg.desc} depends on ${dep.desc} but has a lower required TypeScript version (${pkg.minTypeScriptVersion} < ${dep.minTypeScriptVersion}).`
