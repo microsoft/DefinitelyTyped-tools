7ceb05a4e2814aade7f8c4f5b90b31749a1d8180
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-27 15:01:39 -0700

    More tests

--- packages/eslint-plugin/test/fixtures/types/no-relative-references/index.d.ts
+++ packages/eslint-plugin/test/fixtures/types/no-relative-references/index.d.ts
@@ -5,3 +5,8 @@
 import * as foo from "../foo";
 import * as foo2 from "./v1";
 import * as foo3 from "../foo/v1";
+
+declare module "no-relative-references" {
+  import A = require("no-relative-references/blah"); // Okay; relative
+  import B = require("no-relative-references/v1"); // Bad; versioned subdir
+}
