df63da364cc0d4c0506e4f26d096518b05e84a21
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-26 19:13:52 -0700

    tweak helper

--- packages/eslint-plugin/src/util.ts
+++ packages/eslint-plugin/src/util.ts
@@ -33,30 +33,47 @@ export function commentsMatching(
   }
 }
 
-export function findTypesPackage(file: string) {
-  file = path.resolve(file);
-  let dir = path.dirname(file);
-  const root = path.parse(dir).root;
+export interface TypesPackageInfo {
+  dir: string;
+  packageJson: PackageJSON;
+  realName: string;
+}
 
-  for (; dir !== root; dir = path.dirname(dir)) {
-    try {
-      const packageJsonContents = fs.readFileSync(path.join(dir, "package.json"), "utf8");
-      const packageJson = JSON.parse(packageJsonContents) as {
-        name?: string;
-        version?: string;
-        owners?: string[];
-        devDependencies?: Record<string, string | undefined>;
+export interface PackageJSON {
+  name: string;
+  version: string;
+  owners: string[];
+  devDependencies?: Record<string, string | undefined>;
+}
+
+// TODO(jakebailey): pull this helper out to util package?
+function isTypesPackage(packageJson: Partial<PackageJSON>): boolean {
+  return (
+    typeof packageJson.name === "string" &&
+    packageJson.name.startsWith("@types/") &&
+    typeof packageJson.version === "string" &&
+    Array.isArray(packageJson.owners)
+  );
+}
+
+export function findTypesPackage(file: string): TypesPackageInfo | undefined {
+  file = path.resolve(file);
+  const root = path.parse(file).root;
+
+  for (let dir = path.dirname(file); dir !== root; dir = path.dirname(dir)) {
+    const packageJsonPath = path.join(dir, "package.json");
+    if (!fs.existsSync(packageJsonPath)) {
+      continue;
+    }
+
+    const packageJsonContents = fs.readFileSync(packageJsonPath, "utf8");
+    const packageJson = JSON.parse(packageJsonContents);
+    if (isTypesPackage(packageJson)) {
+      return {
+        dir,
+        packageJson,
+        realName: typesPackageNameToRealName(packageJson.name),
       };
-      // TODO(jakebailey): helper?
-      if (packageJson.name?.startsWith("@types/") && packageJson.version && packageJson.owners) {
-        return {
-          dir,
-          packageJson,
-          realName: typesPackageNameToRealName(packageJson.name),
-        };
-      }
-    } catch {
-      // continue
     }
   }
 
@@ -64,6 +81,7 @@ export function findTypesPackage(file: string) {
 }
 
 export function findDtRoot(typesPackageDir: string) {
+  // TODO(jakebailey): check package.json name instead? pnpm-workspace.yaml?
   let dir = typesPackageDir;
   const root = path.parse(dir).root;
 
