1620ef155b8d1e8f6552c092caadbbf1b04e1f57
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-10-30 10:19:40 -0700

    Move .vscode/settings.json to template, fix prettier formatting and format repo (#790)

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,12 @@ module.exports = {
   modulePathIgnorePatterns: ["packages\\/publisher\\/output"],
   testMatch: ["<rootDir>/packages/*/test/**/*.test.ts", "<rootDir>/packages/dts-critic/index.test.ts"],
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", {
-      tsconfig: "<rootDir>/tsconfig.test.json",
-      diagnostics: false
-    }]
-  }
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.test.json",
+        diagnostics: false,
+      },
+    ],
+  },
 };
