e2aba74a8b29d42a01a009ff5ec2d47364180495
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-01 10:12:35 -0700

    PR feedback

--- packages/eslint-plugin/test/fixtures/types/no-self-import/index.d.ts
+++ packages/eslint-plugin/test/fixtures/types/no-self-import/index.d.ts
@@ -2,8 +2,6 @@ import other from "other-package";
 
 import other from "other-package/this-package";
 
-import myself from "no-selfimport";
-
 import old from "./v1gardenpath"
 
 import old from "./v1verb/other"
