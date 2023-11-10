49f50ba1da0244d4f4b30a0e5ae029cacbb41530
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-09 21:40:57 -0800

    Fix eslint test

--- packages/eslint-plugin/test/no-unnecessary-generics.test.ts
+++ packages/eslint-plugin/test/no-unnecessary-generics.test.ts
@@ -6,7 +6,7 @@ const ruleTester = new ESLintUtils.RuleTester({
   parserOptions: {
     ecmaVersion: 2018,
     tsconfigRootDir: __dirname,
-    project: "./tsconfig.json",
+    project: "./tsconfig.test.json",
   },
   parser: "@typescript-eslint/parser",
 });
