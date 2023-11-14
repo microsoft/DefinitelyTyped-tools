83042b82e015ed170925337437514d23e7436273
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-13 21:00:58 -0800

    Move no-dead-reference

---
+++ packages/eslint-plugin/test/fixtures/types/no-dead-reference/bad.d.ts
@@ -0,0 +1,2 @@
+export class C { }
+/// <reference types="terms" />
---
+++ packages/eslint-plugin/test/fixtures/types/no-dead-reference/bad3.d.ts
@@ -0,0 +1,5 @@
+export class C { }
+/// <reference types="terms" />
+export class D { }
+/// <reference types="multiple" />
+export class E { }
