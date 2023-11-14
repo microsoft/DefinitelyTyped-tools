296005d4cc2e4f21756dfabb49bdb0e211bfef05
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-13 20:13:18 -0800

    Improvements

--- packages/eslint-plugin/test/eslint.test.ts
+++ packages/eslint-plugin/test/eslint.test.ts
@@ -2,26 +2,31 @@ import { ESLint } from "eslint";
 import path from "path";
 import stripAnsi from "strip-ansi";
 import { fixtureRoot } from "./fixtureTester";
+import { globSync } from "glob";
 
-describe("eslint", () => {
-  it("should lint", async () => {
-    const eslint = new ESLint({
-      cwd: fixtureRoot,
-      extensions: [".ts", ".cts", "mts"],
-      overrideConfig: {
-        root: true,
-        extends: ["plugin:@definitelytyped/all"],
-      },
-      useEslintrc: false,
-    });
-    const results = await eslint.lintFiles(["."]);
-    results.sort((a, b) => a.filePath.localeCompare(b.filePath));
-    for (const result of results) {
-      result.filePath = path.relative(fixtureRoot, result.filePath);
-    }
-    const formatter = await eslint.loadFormatter("stylish");
-    const formatted = await formatter.format(results);
-    const resultText = stripAnsi(formatted);
-    expect(resultText).toMatchSnapshot("results");
-  });
+const allFixtures = globSync(["**/*.ts", "**/*.cts", "**/*.mts"], { cwd: fixtureRoot });
+
+let eslint: ESLint;
+
+beforeAll(() => {
+  eslint = new ESLint({ cwd: fixtureRoot });
 });
+
+afterAll(() => {
+  eslint = undefined as any;
+});
+
+for (const fixture of allFixtures) {
+  describe(`fixture ${fixture}`, () => {
+    it("should lint", async () => {
+      const results = await eslint.lintFiles([fixture]);
+      for (const result of results) {
+        result.filePath = path.relative(fixtureRoot, result.filePath);
+      }
+      const formatter = await eslint.loadFormatter("stylish");
+      const formatted = await formatter.format(results);
+      const resultText = stripAnsi(formatted);
+      expect(resultText).toMatchSnapshot("results");
+    });
+  });
+}
