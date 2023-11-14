00370527d4e1cbf16f9e0208e72858c6d802f2d9
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-13 20:43:14 -0800

    Drop unused stuff

--- packages/eslint-plugin/test/fixtures/types/no-any-union/no-any-union-tests.ts
+++ packages/eslint-plugin/test/fixtures/types/no-any-union/no-any-union-tests.ts
@@ -1,9 +1,1 @@
-/// <reference types="devdep"/>
-/// <reference types="otherdevdep"/>
-/// <reference types="other"/>
-
-import other from "other";
-import self from "no-import-of-dev-dependencies";
-
-import devdep from "devdep";
-import * as otherdevdep from "otherdevdep";
+export const z: string | any;
