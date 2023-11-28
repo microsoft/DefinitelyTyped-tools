29792e41eb280a558408fd7648a304dbb3fdd292
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-11-27 16:19:25 -0800

    Drop rc, unused

--- packages/utils/src/typescript-installer.ts
+++ packages/utils/src/typescript-installer.ts
@@ -3,12 +3,9 @@ import * as typeScriptPackages from "@definitelytyped/typescript-packages";
 
 export type TsVersion = TypeScriptVersion | "local";
 
-export function typeScriptPath(version: TsVersion | "next" | "rc", tsLocal: string | undefined): string {
+export function typeScriptPath(version: TsVersion | "next", tsLocal: string | undefined): string {
   if (version === "local") {
     return tsLocal! + "/typescript.js";
-  }
-  if (version === "rc") {
-    throw new Error("unsupported");
   }
   return typeScriptPackages.resolve(version);
 }
