24b4c4010251613b5d5a38ed9680c1f1608a9b4e
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-12-12 19:35:46 -0800

    Remove version set, now that it's strongly typed

--- packages/typescript-packages/src/index.ts
+++ packages/typescript-packages/src/index.ts
@@ -1,10 +1,5 @@
 import { TypeScriptVersion } from "@definitelytyped/typescript-versions";
 
-const versionSet = new Set(TypeScriptVersion.supported);
-
 export function resolve(version: TypeScriptVersion) {
-  if (!versionSet.has(version)) {
-    throw new Error(`Unknown TypeScript version ${version}`);
-  }
   return require.resolve(`typescript-${version}`);
 }
