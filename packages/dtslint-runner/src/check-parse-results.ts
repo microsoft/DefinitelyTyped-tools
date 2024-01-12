83421ad1e975e2f7cd71fb618b2ceb19bd6da69b
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2024-01-12 13:10:07 -0800

    Loosen checkParseResults
    1. Don't require types dependencies to be on DT.
    2. Allow types dependencies to be pinned.
    3. Don't bother asserting that dependencies from package.json are in
    fact present in package.json.
    
    The remaining check requires dependents to have a higher-or-equal
    minTypescriptVersion compared to their dependency. This should really be
    in dtslint, but I don't want to make the fix (too) complex. And I haven't gone
    to check whether dtslint has access to `allPackages`.

--- packages/dtslint-runner/src/check-parse-results.ts
+++ packages/dtslint-runner/src/check-parse-results.ts
@@ -13,13 +13,6 @@ export async function checkParseResults(allPackages: AllPackages): Promise<strin
   const errors = [];
   for (const pkg of await allPackages.allTypings()) {
     for await (const dep of allPackages.allDependencyTypings(pkg)) {
-      // check raw version because parsed version doesn't currently retain range information
-      const version = assertDefined(new Map(pkg.allPackageJsonDependencies()).get(dep.name));
-      if (semver.parse(version)) {
-        errors.push(
-          `${pkg.desc}'s dependency on ${dep.desc}@${version} must use "*" or a version range, not a specific version.`,
-        );
-      }
       if (dep.minTypeScriptVersion > pkg.minTypeScriptVersion) {
         errors.push(
           `${pkg.desc} depends on ${dep.desc} but has a lower required TypeScript version (${pkg.minTypeScriptVersion} < ${dep.minTypeScriptVersion}).`,
