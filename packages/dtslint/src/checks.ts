c3355d3b0431c9888999bf3a52d64b141c9f084b
Andrew Branch <andrewbranch@users.noreply.github.com>
2024-01-25 13:09:24 -0800

    Fix execFileSync format
    Co-authored-by: Jake Bailey <5341706+jakebailey@users.noreply.github.com>

--- packages/dtslint/src/checks.ts
+++ packages/dtslint/src/checks.ts
@@ -145,7 +145,7 @@ export function runAreTheTypesWrong(dirPath: string, implementationTarballPath: 
   const attwPackageJsonPath = require.resolve("@arethetypeswrong/cli/package.json");
   const attwBinPath = joinPaths(dirname(attwPackageJsonPath), readJson(attwPackageJsonPath).bin.attw);
   const npmPath = which.sync("pnpm", { nothrow: true }) || which.sync("npm");
-  execFileSync(`${npmPath} pack`, { cwd: dirPath, stdio: "ignore", env: { ...process.env, COREPACK_ENABLE_STRICT: "0" } });
+  execFileSync(npmPath, ["pack"], { cwd: dirPath, stdio: "ignore", env: { ...process.env, COREPACK_ENABLE_STRICT: "0" } });
   try {
     const output = execFileSync(
       attwBinPath,
