41a064c8f62b70d99ee6f69eb8cc3b9d1336f4b3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-01-29 21:08:31 -0800

    Re-allow packages without tests

--- packages/dtslint/src/checks.ts
+++ packages/dtslint/src/checks.ts
@@ -49,9 +49,9 @@ export function checkTsconfig(dirPath: string, config: Tsconfig): string[] {
     if (!config.files.includes("index.d.ts")) {
       errors.push('"files" list must include "index.d.ts".');
     }
-    if (!config.files.some((f) => /(?:\.[cm]?ts|\.tsx)$/.test(f) && !isDeclarationPath(f))) {
-      errors.push('"files" list must include at least one ".ts", ".tsx", ".mts" or ".cts" file for testing.');
-    }
+    // if (!config.files.some((f) => /(?:\.[cm]?ts|\.tsx)$/.test(f) && !isDeclarationPath(f))) {
+    //   errors.push('"files" list must include at least one ".ts", ".tsx", ".mts" or ".cts" file for testing.');
+    // }
   }
 
   for (const key of Object.getOwnPropertyNames(mustHave) as (keyof typeof mustHave)[]) {
