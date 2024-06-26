ee9d00b212cbb3dec72089dd09f87a72221edc0a
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-06-26 13:46:53 -0700

    Fix up test to be more accurate

--- packages/eslint-plugin/test/fixtures/types/expect-error-range/expect-error-range-tests.ts
+++ packages/eslint-plugin/test/fixtures/types/expect-error-range/expect-error-range-tests.ts
@@ -1,21 +1,19 @@
+// In TypeScript <5.5, elem will have type "string",
+// but 5.5 it will be "string | number".
 const elem = ["value", undefined].filter(x => x != null)[0];
-declare const test: typeof elem & undefined
 
 
-// These should give expect errors:
+// This should error in 5.4, but not 5.5.
+const test1a: string = elem;
 
-const isStringNoIgnore: string = elem;
-
-test?.trim();
-
+// This should error in 5.5, but not 5.4.
+const test1b: undefined extends typeof elem ? typeof elem : never = elem;
 
 
-// These should give expect these should not:
-
+// None of these expects should error.
 
 // @ts-expect-error <5.5
-const isStringIgnore: string = elem;
-
+const test2a: string = elem;
 
 // @ts-expect-error >=5.5
-test?.trim();
+const test2b: undefined extends typeof elem ? typeof elem : never = elem;
