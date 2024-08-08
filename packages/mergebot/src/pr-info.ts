b52e734ae124cd02a82c5c256c34bd5f79864ad5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-08-08 13:43:49 -0700

    Fix column name check

--- packages/mergebot/src/pr-info.ts
+++ packages/mergebot/src/pr-info.ts
@@ -1,4 +1,4 @@
-import { BlessedColumnName, ColumnName, columnNameToBlessed, PopularityLevel, projectBoardNumber } from "./basic";
+import { BlessedColumnName, ColumnName, columnNameToBlessed, isBlessedColumnName, PopularityLevel, projectBoardNumber } from "./basic";
 import {
   PR_repository_pullRequest,
   PR_repository_pullRequest_commits_nodes_commit_checkSuites,
@@ -309,7 +309,7 @@ function getLastMaintainerBlessing(
   const card = pr.projectItems.nodes?.find((card) => card?.project.number === projectBoardNumber);
   const columnName =
     card?.fieldValueByName?.__typename === "ProjectV2ItemFieldSingleSelectValue" && card.fieldValueByName.name;
-  if (columnName === "Waiting for Code Reviews (Blessed)" && card?.updatedAt) {
+  if (columnName && isBlessedColumnName(columnName) && card?.updatedAt) {
     // Normally relying on the updatedAt of the card is not reliable, but in this case it's fine
     // becuase the bot will never move the card into the blessed state, only out of it.
     // If the card is already in a blessed state, the bot will not mutate the card.
