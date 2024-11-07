35d32c577312b71cc1eb2bd4fa18c1bbb07cf8ba
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-11-07 15:17:48 -0800

    Fix cache being checked in by changesets

--- packages/publisher/test/isAlreadyDeprecated.test.ts
+++ packages/publisher/test/isAlreadyDeprecated.test.ts
@@ -2,7 +2,7 @@ import { NotNeededPackage } from "@definitelytyped/definitions-parser";
 import { isAlreadyDeprecated } from "../src/calculate-versions";
 
 describe("isAlreadyDeprecated", () => {
-  const shouldSkip = !process.env.CI;
+  const shouldSkip = !process.env.GITHUB_ACTIONS;
 
   (shouldSkip ? it.skip : it)("should report @types/commander as deprecated", async () => {
     const pkg = new NotNeededPackage("@types/commander", "commander", "2.12.2");
