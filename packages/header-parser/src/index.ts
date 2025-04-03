6ae14288b0e68bbae30faaf910c10d478f4dc354
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-04-03 08:28:29 -0700

    Comment out instead

--- packages/header-parser/src/index.ts
+++ packages/header-parser/src/index.ts
@@ -340,7 +340,9 @@ export function getTypesVersions(dirPath: string): readonly AllTypeScriptVersion
     if (!TypeScriptVersion.isTypeScriptVersion(version)) {
       throw new Error(`There is an entry named ${name}, but ${version} is not a valid TypeScript version.`);
     }
-
+    // if (!TypeScriptVersion.isSupported(version)) {
+    //   throw new Error(`At ${dirPath}/${name}: TypeScript version ${version} is not supported on Definitely Typed.`);
+    // }
     return version;
   });
 }
